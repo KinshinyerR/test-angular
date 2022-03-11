b0c430cf5010ac07dc7c4064ebe2ea06191dd472
KinshinyerR <kinshinyer02@gmail.com>
2022-03-11 01:57:43 -0500

    add the logic of the app

--- src/app/Components/table/table.component.ts
+++ src/app/Components/table/table.component.ts
@@ -1,15 +1,94 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { CoinsService } from 'src/app/Services/coins.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.css']
+  styleUrls: ['./table.component.css'],
+  providers: [CoinsService]
 })
 export class TableComponent implements OnInit {
+  [x:string]:any;
+  
+  public tableForm:FormGroup | undefined;
+  public resultCrypto:any[]=[];
+  public resultCryptoSelect:any[]=[];
+  public resultCryptoEUR:any[]=[];
+  public resultCryptoCOP:any[]=[];
+  public resultCryptoToday:any[]=[];
+  public dates:any[]=[];
 
-  constructor() { }
+  constructor(private serviceCoins: CoinsService) { }
 
   ngOnInit(): void {
+    this.getDates();
+    this.priceToday();
+    this.getInfoCoinToday();
   }
 
+  getDates(){
+    for (let i = 0; i < 15; i++) {
+      const today = new Date();
+      const yesterday = new Date(today);
+      yesterday.setDate(yesterday.getDate() - i);
+      const newDate = new DatePipe('en-CO').transform(yesterday, 'yyyy-MM-dd');
+      this.dates.push(newDate)
+    }
+    this.getInfoCoin();
+  }
+
+  getInfoCoinToday(){
+    setInterval(this.priceToday, 60000);
+  }
+
+  priceToday(){
+    const today = new Date();
+    const newDate = new DatePipe('en-CO').transform(today, 'yyyy-MM-dd');
+    this.serviceCoins.getCrypto(newDate? newDate : "").subscribe(result => {
+      console.log(result[0]);
+      this.resultCryptoToday.push(Object.assign(result[0], {date:newDate}));
+    });
+  }
+
+  getInfoCoin(){
+    for (let i = 0; i < this.dates.length; i++) {
+      this.serviceCoins.getCrypto(this.dates[i]).subscribe(result => {
+        console.log(result[0]);
+        this.resultCrypto.push(Object.assign(result[0], {date:this.dates[i]}));
+      });
+    }
+    this.getInfoCoinToday();
+  }
+
+  selectCoin(target: any){
+    this.resultCryptoSelect = [];
+    this.resultCryptoEUR = [];
+    this.resultCryptoCOP = [];
+
+    let info = {target};
+    let date = info.target.innerText;
+    let endDate = date.substring(6,16);
+
+    this.serviceCoins.getCrypto(endDate).subscribe(result => {
+      console.log(result[0]);
+      this.resultCryptoSelect.push(Object.assign(result[0], {date:endDate}));
+    });
+    this.serviceCoins.getCryptoEUR(endDate).subscribe(result => {
+      console.log(result[0]);
+      this.resultCryptoEUR.push(Object.assign(result[0], {date:endDate}));
+    });
+    this.serviceCoins.getCryptoCOP(endDate).subscribe(result => {
+      console.log(result[0]);
+      this.resultCryptoCOP.push(Object.assign(result[0], {date:endDate}));
+    });
+  }
+
+  buttonDate(){
+    const date = new FormControl('',[]);
+    this.tableForm = new FormGroup({
+      date:date,
+    })
+  }
 }
