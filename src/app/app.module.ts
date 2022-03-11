635baa8bc67849da9e3459d827a439fc32b96e84
KinshinyerR <kinshinyer02@gmail.com>
2022-03-11 01:54:30 -0500

    add the module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CardComponent } from './Components/card/card.component';
 import { TableComponent } from './Components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
     TableComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
